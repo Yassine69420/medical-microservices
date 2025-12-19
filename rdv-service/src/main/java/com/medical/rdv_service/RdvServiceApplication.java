8128702ad42d542c5476d2082951d910adde00f1
Yassine Slimani <yassineslimani2003@gmail.com>
2025-12-19 16:35:25 +0100

    first commit

---
+++ rdv-service/src/main/java/com/medical/rdv_service/RdvServiceApplication.java
@@ -0,0 +1,13 @@
+package com.medical.rdv_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdvServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RdvServiceApplication.class, args);
+	}
+
+}
---
+++ rdv-service/src/test/java/com/medical/rdv_service/RdvServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medical.rdv_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdvServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
